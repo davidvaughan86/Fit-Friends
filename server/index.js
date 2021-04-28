3c8881fdf696b6e1aa37b070e2e01c6bcc00254b
davidvaughan86 <david.vaughan86@gmail.com>
2021-04-28 05:20:28 -0400

    fetch

--- server/index.js
+++ server/index.js
@@ -2,16 +2,37 @@ const express = require('express');
 const app = express();
 const port = 6006
 const cors = require('cors')
-const es6render = require('express-es6-template-engine')
+const es6Renderer = require("express-es6-template-engine");
 
-app.use(express(cors()))
+app.use(express.json())
+app.use(cors())
 
-app.set('html', 'es6render')
-app.set('views','' )
+app.engine('html', es6Renderer)
+app.set('views','./templates' )
+app.set('view engine', 'html')
 
 
 app.get('/' , (req,res) => {
-    res.render()
+    res.render('home')
+})
+
+app.post('/new_friends' , async (req,res) => {
+    const {
+        fName,
+        lName,
+        gymName
+    } = req.body
+    const addFriendsToDB = await pool.query(
+        'INSERT INTO friends (firstName, lastName, gymName) VALUES ($1, $2, $3)', [fName, lName, gymName]
+    );
+
+
+    res.status(200).send('friends are added')
+    
+})
+
+app.get('/addfriends' , (req,res) => {
+    res.render('addfriends')
 })
 
 
